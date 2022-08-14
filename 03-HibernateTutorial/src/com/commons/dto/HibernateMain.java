be9c6532e85a2c5b10f5efaa64c1b3ae3057faa8
meena <meena@localhost>
2022-08-14 20:53:51 +0530

    first commit

---
+++ 03-HibernateTutorial/src/com/commons/dto/HibernateMain.java
@@ -0,0 +1,22 @@
+package com.commons.dto;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.commons.entity.UserDetail;
+
+public class HibernateMain {
+
+	public static void main(String[] arg) {
+		UserDetail user = new UserDetail();
+		user.setUserId(1);
+		user.setUserName("first user");
+		
+		SessionFactory sf = new Configuration().configure().buildSessionFactory();
+		Session session = sf.openSession();
+		session.beginTransaction();
+		session.save(user);
+		session.getTransaction().commit();
+	}
+}
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# HibernateTutorial-
+# HibernateTutorial
+
+video:
+https://www.youtube.com/watch?v=Yv2xctJxE-w&list=PL4AFF701184976B25&index=1
+
+Download links:
+https://sourceforge.net/projects/hibernate/files/hibernate3/3.6.10.Final/
+
+
